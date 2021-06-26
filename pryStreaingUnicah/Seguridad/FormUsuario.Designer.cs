4c95d68cc4aababb3608475ff9301efa1992abf3
gustavo-exe <gus.elim2@gmail.com>
2021-06-26 13:54:57 -0600

    Crear un usuario nuevo.

---
+++ pryStreaingUnicah/Seguridad/FormUsuario.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace pryStreaingUnicah.Seguridad
+{
+    partial class FormUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.chkEstado = new System.Windows.Forms.CheckBox();
+            this.cmbPerfil = new System.Windows.Forms.ComboBox();
+            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.dpFechaNac = new System.Windows.Forms.DateTimePicker();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 365);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 33;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(422, 365);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 32;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(331, 365);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 31;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // chkEstado
+            // 
+            this.chkEstado.AutoSize = true;
+            this.chkEstado.Location = new System.Drawing.Point(331, 273);
+            this.chkEstado.Name = "chkEstado";
+            this.chkEstado.Size = new System.Drawing.Size(142, 17);
+            this.chkEstado.TabIndex = 30;
+            this.chkEstado.Text = "Habilitado/Deshabilitado";
+            this.chkEstado.UseVisualStyleBackColor = true;
+            // 
+            // cmbPerfil
+            // 
+            this.cmbPerfil.FormattingEnabled = true;
+            this.cmbPerfil.Location = new System.Drawing.Point(331, 237);
+            this.cmbPerfil.Name = "cmbPerfil";
+            this.cmbPerfil.Size = new System.Drawing.Size(249, 21);
+            this.cmbPerfil.TabIndex = 29;
+            // 
+            // txtContrasenia
+            // 
+            this.txtContrasenia.Location = new System.Drawing.Point(331, 311);
+            this.txtContrasenia.Name = "txtContrasenia";
+            this.txtContrasenia.PasswordChar = '*';
+            this.txtContrasenia.Size = new System.Drawing.Size(249, 20);
+            this.txtContrasenia.TabIndex = 28;
+            this.txtContrasenia.UseSystemPasswordChar = true;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(331, 193);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(249, 20);
+            this.txtCorreo.TabIndex = 27;
+            // 
+            // dpFechaNac
+            // 
+            this.dpFechaNac.Location = new System.Drawing.Point(331, 153);
+            this.dpFechaNac.Name = "dpFechaNac";
+            this.dpFechaNac.Size = new System.Drawing.Size(200, 20);
+            this.dpFechaNac.TabIndex = 26;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(331, 109);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(249, 20);
+            this.txtNombre.TabIndex = 25;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(331, 62);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(249, 20);
+            this.txtUsuario.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Contrasenia";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Estado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Perfil";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Correo electronico";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Fecha nacimiento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Nombre completo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Usuario";
+            // 
+            // FormUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.chkEstado);
+            this.Controls.Add(this.cmbPerfil);
+            this.Controls.Add(this.txtContrasenia);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.dpFechaNac);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormUsuario";
+            this.Text = "FormUsuario";
+            this.Load += new System.EventHandler(this.FormUsuario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.CheckBox chkEstado;
+        private System.Windows.Forms.ComboBox cmbPerfil;
+        private System.Windows.Forms.TextBox txtContrasenia;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.DateTimePicker dpFechaNac;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ pryStreaingUnicah/Seguridad/FormUsuario.cs
@@ -0,0 +1,106 @@
+﻿using pryStreaingUnicah.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryStreaingUnicah.Seguridad
+{
+    public partial class FormUsuario : Form
+    {
+        StreamingUnicahIdentidades entity = new StreamingUnicahIdentidades();
+        public FormUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUsuario_Load(object sender, EventArgs e)
+        {
+            SelectWhereEstado();
+        }
+
+        private void SelectWhereEstado()
+        {
+            var tablaPerfil = from p in entity.Perfiles
+                              where p.EstadoPerfil == true
+                              select new
+                              {
+                                  p.IdPerfil,
+                                  p.DescripcionPerfil
+                              };
+            DataTable dataTablePerfil = tablaPerfil.CopyAnonymusToDataTable();
+
+            cmbPerfil.DataSource = dataTablePerfil;
+            cmbPerfil.DisplayMember = dataTablePerfil.Columns[1].ColumnName;
+            cmbPerfil.ValueMember = dataTablePerfil.Columns[0].ColumnName;
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Validaciones
+            if (txtUsuario.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese el usuario");
+                return;
+            }
+
+            if (txtNombre.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese el nombre completo");
+                return;
+            }
+
+            if (dpFechaNac.Value == null || dpFechaNac.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese la fecha de nacimiento");
+                return;
+            }
+
+            if (cmbPerfil.SelectedIndex == -1)
+            {
+                MessageBox.Show("Seleccione el perfil");
+                return;
+            }
+
+            if (txtContrasenia.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese Contrasenia");
+                return;
+            }
+
+            //Insertar usuario
+            try
+            {
+                //Intancia de indetidad
+                Usuarios tablaUsuarios = new Usuarios();
+                tablaUsuarios.Usuario = txtUsuario.Text;
+                tablaUsuarios.NombreCompleto = txtNombre.Text;
+                tablaUsuarios.FechaNacimeinto = dpFechaNac.Value;
+                tablaUsuarios.FKPerfil = Convert.ToInt16(cmbPerfil.SelectedValue);
+                tablaUsuarios.CorreoElectronico = txtCorreo.Text;
+                tablaUsuarios.Estdo = chkEstado.Checked;
+                
+                //Encriptacion
+                Hash hash = new Hash();
+                tablaUsuarios.Contrasenia = hash.obtenerHash256(txtContrasenia.Text);
+
+                //Guardado de cambios
+                entity.Usuarios.Add(tablaUsuarios);
+                entity.SaveChanges();
+                MessageBox.Show("Datos guardados");
+
+                //Limpieza de campos
+                txtContrasenia.Text = txtCorreo.Text = txtNombre.Text = txtUsuario.Text = string.Empty;
+                chkEstado.Checked = false;
+                dpFechaNac.Value = DateTime.Today;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
